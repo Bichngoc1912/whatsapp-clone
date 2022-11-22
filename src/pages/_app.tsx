6c9945a9ba0a18acd1ee8b5b80b77df856e3f7a8
Bichngoc1912 <bichngoc3071998@gmail.com>
2022-11-23 00:31:30 +0700

    detect loggin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import styles from '@@/styles/Home.module.css';
 import type { AppProps } from 'next/app';
+import LoginPage from './auth/login';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const loggedInUser = false;
+
+  if (!loggedInUser) return <LoginPage />
+  
   return <Component {...pageProps} />;
 }
